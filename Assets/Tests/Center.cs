d414ed7f6e27c85fedcc3bc721cb48b3db702033
Patrice Vignola <vignola.patrice@gmail.com>
2015-08-12 13:48:16 -0400

    - Added the links highlighting when hovering on a node and stopping the   sphere rotation when hovering over it

--- Assets/Tests/Center.cs
+++ Assets/Tests/Center.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Sphere : MonoBehaviour
+public class Center : MonoBehaviour
 {
     public FriendshipLink Link;
-    public GameObject SpherePrototype;
+    public PersonTest Person;
     public int PointsAmount = 50;
     public float SphereRadius = 1f;
     public float rotationSpeed = 0.7f;
 
-    private GameObject[] nodes;
+    private PersonTest[] people;
 
     void Awake()
     {
@@ -19,7 +20,17 @@ public class Sphere : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(Vector3.one * Time.deltaTime * rotationSpeed);
+        Vector3 screenMousePos = Input.mousePosition;
+
+        screenMousePos.z = transform.position.z - Camera.main.transform.position.z;
+
+        Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(screenMousePos);
+
+        // If the world position of the mouse is greater than the radius of the sphere, we are outside
+        if (Mathf.Sqrt(worldMousePos.x * worldMousePos.x + worldMousePos.y * worldMousePos.y) > SphereRadius + 1f)
+        {
+            transform.Rotate(Vector3.one * Time.deltaTime * rotationSpeed);
+        }
     }
 
     private void InstantiateNodes()
@@ -30,7 +41,7 @@ public class Sphere : MonoBehaviour
          * dont end up drawing clusters of points and the spirals are way
          * less visible.
          */
-        nodes = new GameObject[PointsAmount];
+        people = new PersonTest[PointsAmount];
 
         float goldenAngle = Mathf.PI * (3 - Mathf.Sqrt(5));
 
@@ -45,11 +56,11 @@ public class Sphere : MonoBehaviour
             float x = Mathf.Sin(longitude) * r;
             float y = Mathf.Cos(longitude) * r;
 
-            GameObject simon = Instantiate(SpherePrototype, new Vector3(x, y, z), Quaternion.identity) as GameObject;
+            PersonTest simon = Instantiate(Person, new Vector3(x, y, z), Quaternion.identity) as PersonTest;
 
             simon.transform.parent = this.transform;
 
-            nodes[i] = simon;
+            people[i] = simon;
 
             z -= zDistance;
             longitude += goldenAngle;
@@ -58,13 +69,17 @@ public class Sphere : MonoBehaviour
 
     private void AssignLinks()
     {
-        for (int i = 0; i < nodes.Length / 4; i++)
+        for (int i = 0; i < people.Length / 4; i++)
         {
             FriendshipLink link = Instantiate(Link) as FriendshipLink;
 
-            int destinationIndex = Random.Range(nodes.Length / 2, nodes.Length - 1);
+            int destinationIndex = Random.Range(people.Length / 2, people.Length - 1);
 
-            link.AttachToObjects(nodes[i], nodes[destinationIndex]);
+            link.AttachToObjects(people[i].gameObject, people[destinationIndex].gameObject);
+
+            // Temporary stuff, for testing
+            people[i].AddLink(link);
+            people[destinationIndex].AddLink(link);
         }
     }
 }
---
+++ Assets/Tests/PersonTest.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Collider))]
+public class PersonTest : MonoBehaviour
+{
+    // Temporary, for test
+    private List<FriendshipLink> _links;
+    private bool _highlighted = false;
+
+    void Awake()
+    {
+        _links = new List<FriendshipLink>();
+    }
+
+    public void AddLink(FriendshipLink link)
+    {
+        _links.Add(link);
+    }
+
+    void OnMouseOver()
+    {
+        if (!_highlighted)
+        {
+            _highlighted = true;
+
+            foreach (FriendshipLink link in _links)
+            {
+                link.Highlight(true, 1f);
+            }
+        }
+    }
+
+    void OnMouseExit()
+    {
+        _highlighted = false;
+
+        foreach (FriendshipLink link in _links)
+        {
+            link.Highlight(false, 1f);
+        }
+    }
+}
