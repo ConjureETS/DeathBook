fd2aea25c1ea730434db1632b28e6da92e1addc9
RosimInc <rosim_inc@hotmail.com>
2015-08-16 19:23:42 -0400

    Started implementing working game strategies.
    Signed-off-by: RosimInc <rosim_inc@hotmail.com>

--- Assets/Scripts/Models/GameStrategy.cs
+++ Assets/Scripts/Models/GameStrategy.cs
@@ -14,6 +14,7 @@ namespace DeathBook.Model
 			public Vector3 friendRatio = new Vector3();
 			public Vector3 lastDeath = new Vector3();
 			public float maxDeathDuration = 24 * 60 * 7;
+			public float modifier = 0.1f;
 		}
 		
 		[System.Serializable]
@@ -23,6 +24,7 @@ namespace DeathBook.Model
 			public Vector3 importance = new Vector3();
 			public Vector3 numFriends = new Vector3();
 			public Vector3 awareness = new Vector3();
+			public float modifier = 0.1f;
 		}
 		
 		[System.Serializable]
@@ -31,6 +33,7 @@ namespace DeathBook.Model
 			public Vector3 popularity = new Vector3();
 			public Vector3 importance = new Vector3();
 			public Vector3 frequency = new Vector3();
+			public float modifier = 0.1f;
 		}
 
 
@@ -40,14 +43,14 @@ namespace DeathBook.Model
 
 		public float GetAwarenessChange(int numDeadFriends, int numAliveFriends, int sinceLastDeath)
 		{
-			float friendsRatioInd = numAliveFriends == 0 ? 1 : GetValue(Mathf.Min(numDeadFriends / numAliveFriends, 1), awarenessChange.friendRatio);
+			float friendsRatioInd = numAliveFriends == 0 ? 1 : GetValue(Mathf.Min(numDeadFriends * 1f / numAliveFriends, 1f), awarenessChange.friendRatio);
 			
 			float max = awarenessChange.maxDeathDuration;
 			float lastDeathInd = GetValue(1 - Mathf.Min(sinceLastDeath, max) / max, awarenessChange.lastDeath);
 
-			//Debug.Log(lastDeathInd + "    " + friendsRatioInd);
+			Debug.Log(friendsRatioInd + "  --  " + lastDeathInd);
 			
-			return Mathf.Clamp(friendsRatioInd * lastDeathInd, 0, 1);
+			return Mathf.Clamp(friendsRatioInd * lastDeathInd, 0, 1) * awarenessChange.modifier;
 		}
 
 		public float GetDeathNoticing(float friendPopularity, float friendshipImportance, int numAliveFriends, float awareness)
@@ -56,12 +59,11 @@ namespace DeathBook.Model
 
 			float importanceInd = GetValue(friendshipImportance, deathNoticing.importance);
 
-			//TODO Should be inverse and there should be an expected number of friends
-			float aliveFriendsInd = GetValue(numAliveFriends, deathNoticing.numFriends);
+			float aliveFriendsInd = GetValue(Mathf.Min(numAliveFriends * 0.1f, 1), deathNoticing.numFriends);
 
 			float awarenessInd = GetValue(awareness, deathNoticing.awareness);
 
-			return Mathf.Clamp(popularityInd * importanceInd * aliveFriendsInd * awarenessInd, 0, 1); 
+			return Mathf.Clamp(popularityInd * importanceInd * aliveFriendsInd * awarenessInd, 0, 1) * deathNoticing.modifier; 
 		}
 
 		public float GetChanceToPost(float friendPopularity, float friendshipImportance)
@@ -70,7 +72,7 @@ namespace DeathBook.Model
 
 			float importanceInd = GetValue(1 - friendshipImportance, deathNoticing.importance);
 
-			return Mathf.Clamp(popularityInd * importanceInd, 0, 1); 
+			return Mathf.Clamp(popularityInd * importanceInd, 0, 1) * chanceToPost.modifier; 
 		}
 
 		public float GetChanceToRead()
---
+++ Assets/Tests/RulesTest.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using DeathBook.Model;
+
+public class RulesTest : MonoBehaviour {
+
+	public GameStrategy strategy = new GameStrategy();
+	public ChangeAwarenessTest awareness = new ChangeAwarenessTest();
+	public NoticeDeathTest noticeDeath = new NoticeDeathTest();
+
+	public static GameStrategy str = null;
+
+	[System.Serializable]
+	public class ChangeAwarenessTest
+	{
+		public int numDeadFriends;
+		public int numAliveFriends;
+		public int sinceLastDeath;
+		public bool test = false;
+		public float result = 0f;
+
+		public void Update()
+		{
+			if (!test)
+				return;
+
+			test = false;
+
+			result = RulesTest.str.GetAwarenessChange(numDeadFriends, numAliveFriends, sinceLastDeath);
+		}
+	}
+
+	[System.Serializable]
+	public class NoticeDeathTest 
+	{
+		public float popularity;
+		public float importance;
+		public int numAliveFriends;
+		public float awareness;
+
+		public bool test = false;
+		public float result = 0f;
+
+		public void Update()
+		{
+			if (!test)
+				return;
+
+			test = false;
+
+			result = RulesTest.str.GetDeathNoticing(popularity, importance, numAliveFriends, awareness);
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		str = strategy;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		awareness.Update();
+	}
+}
