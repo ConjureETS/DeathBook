f8d9c4e6060bfa3db752ea6eb6d3222c6a253217
unknown <conjure@CONJURE043616.ens.ad.etsmtl.ca>
2015-08-16 12:29:23 -0400

    splash script

---
+++ Assets/Scripts/loadLevel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadLevel : MonoBehaviour
+{
+    public int timeToWait = 2;
+    // Use this for initialization
+
+    void Start()
+    {
+        Invoke("NextScene", timeToWait);
+    }
+
+    void NextScene()
+    {
+        Application.LoadLevel("Gameplay");
+    }
+
+}
