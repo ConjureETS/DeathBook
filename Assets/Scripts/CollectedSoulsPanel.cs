04c9c9911e177ee91a1403db8ba4d0d3eeebef0d
Patrice Vignola <vignola.patrice@gmail.com>
2015-08-17 00:03:15 -0400

    - Added the collected souls

---
+++ Assets/Scripts/CollectedSoulsPanel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using DeathBook.Model;
+
+public class CollectedSoulsPanel : MonoBehaviour
+{
+    public Text CollectedSouls;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        CollectedSouls.text = LevelManager.Instance.GameLevel.NumDead.ToString();
+	}
+}
