efc567e8aacfec91c00fdb53c15a0d799933a637
RosimInc <rosim_inc@hotmail.com>
2015-08-16 23:43:23 -0400

    Fix, forgot the new script
    Signed-off-by: RosimInc <rosim_inc@hotmail.com>

---
+++ Assets/Scripts/Models/ShoutBubble.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace DeathBook.Model
+{
+	public class ShoutBubble
+	{
+		private int startTime;
+		public int StartTime { get { return startTime; } }
+		private int endTime;
+		public int EndTime { get { return endTime; } }
+		private Friendship friends;
+		public Friendship Friends { get { return friends; } }
+
+		public ShoutBubble(int startTime, Friendship friendship)
+		{
+			this.startTime = startTime;
+			this.endTime = startTime + (int) Utils.GetRandomValue(700, 250, 3);
+			friends = friendship;
+		}
+	}
+}
